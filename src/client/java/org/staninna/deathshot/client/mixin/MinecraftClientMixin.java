3599f3f17d6db57706b1c1e1ab59d96b3a8e9879
Staninna <26458805+Staninna@users.noreply.github.com>
2024-12-17 17:09:58 +0100

    it compiles

---
+++ src/client/java/org/staninna/deathshot/client/mixin/MinecraftClientMixin.java
@@ -0,0 +1,36 @@
+package org.staninna.deathshot.client.mixin;
+
+import org.spongepowered.asm.mixin.Unique;
+import org.staninna.deathshot.client.DeathshotClient;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.DeathScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+
+    @Unique @Shadow @Nullable public Screen currentScreen;
+    private boolean tookDeathShot;
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    public void onTick(CallbackInfo ci) {
+        ClientPlayerEntity player = MinecraftClient.getInstance().player;
+        if(player == null)
+            return;
+        if(!tookDeathShot && player.isDead()) {
+            if(player.showsDeathScreen() && !(currentScreen instanceof DeathScreen))
+                return;
+            tookDeathShot = true;
+            DeathshotClient.saveScreenShot();
+        } else if(tookDeathShot && !player.isDead()) {
+            tookDeathShot = false;
+        }
+    }
+}
---
+++ src/main/java/org/staninna/deathshot/Deathshot.java
@@ -0,0 +1,10 @@
+package org.staninna.deathshot;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Deathshot implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
